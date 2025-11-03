fae812c9eca846b61d9be966e5580ed93d63e8bf
Pranav Sajeev <psaj0002@student.monash.edu>
2025-11-03 21:00:54 +1100

    Updating run_app documentation

--- R/run_app.R
+++ R/run_app.R
@@ -1,4 +1,8 @@
 #' Launch the Shiny App
+#'
+#' This function launches the COVID-19 Outbreak Risk Explorer Shiny app
+#' included in the CovidDataPackage1 package.
+#'
 #' @export
 run_app <- function() {
   app_dir <- system.file("app", package = "CovidDataPackage1")
